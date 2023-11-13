8e3bfeea0a4e9e8a24acb7bd38263bc67ff9e20d
Jonas-Karl <jonas.karl.rade@gmail.com>
2023-11-13 12:50:16 +0100

    revert sentDate encapsulation

--- src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/email/Email.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/email/Email.java
@@ -9,7 +9,6 @@ import de.dhbw.karlsruhe.students.mailflow.core.domain.email.entities.Attachment
 import de.dhbw.karlsruhe.students.mailflow.core.domain.email.entities.Header;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.email.entities.Recipients;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects.EmailId;
-import de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects.SentDate;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.common.models.AggregateRoot;
 
 /**
@@ -22,12 +21,12 @@ public final class Email extends AggregateRoot<EmailId> {
     private Recipients recipients;
 
     private String content;
-    private SentDate sentDate;
+    private Date sentDate;
     private boolean isRead;
     private List<Header> headers;
     private Set<Attachment> attachments;
 
-    private Email(EmailId id, String subject, Address sender, Recipients recipients, String content, SentDate sentDate,
+    private Email(EmailId id, String subject, Address sender, Recipients recipients, String content, Date sentDate,
             List<Header> headers, Set<Attachment> attachments) {
         super(id);
         this.subject = subject;
@@ -35,13 +34,12 @@ public final class Email extends AggregateRoot<EmailId> {
         this.recipients = recipients;
         this.content = content;
         this.sentDate = sentDate;
-        this.sentDate = new SentDate(new Date(0));
         this.headers = headers;
         this.attachments = attachments;
         this.isRead = false;
     }
 
-    public static Email create(String subject, Address sender, Recipients recipients, String content, SentDate sentDate,
+    public static Email create(String subject, Address sender, Recipients recipients, String content, Date sentDate,
             List<Header> headers, Set<Attachment> attachments) {
         return new Email(EmailId.createUnique(), subject, sender, recipients, content, sentDate, headers, attachments);
     }
@@ -62,7 +60,7 @@ public final class Email extends AggregateRoot<EmailId> {
         return content;
     }
 
-    public SentDate getSentDate() {
+    public Date getSentDate() {
         return sentDate;
     }
 
--- src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/email/value_objects/SentDate.java
+++
@@ -1,6 +0,0 @@
-package de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects;
-
-import java.util.Date;
-
-public record SentDate(Date date) {
-}
