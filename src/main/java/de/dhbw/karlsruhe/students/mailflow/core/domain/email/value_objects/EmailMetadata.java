0e0f607e32bb9d423a5a67985778822099c892cd
Jonas-Karl <jonas.karl.rade@gmail.com>
2023-11-17 10:34:15 +0100

    missing doc

--- src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/email/value_objects/EmailMetadata.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/email/value_objects/EmailMetadata.java
@@ -1,6 +1,6 @@
 package de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects;
 /**
- * 
+ * Representation of constant e-mail, which will exist with the creation of an e-mail
  */
 public record EmailMetadata(Subject subject, Address sender, Recipients recipients, SentDate sentDate) {
 }
