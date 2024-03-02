5398d2a5069f8131c00f0957d0ffcd4110a76536
jens1o <mail@jens-hausdorf.de>
2024-03-02 21:32:21 +0100

    feat(imap): Allow clients to log in (will always succeed)
    now we're up to:
    * OK JAMES IMAP4rev1 Server dm-FXPYNP4K5J is ready.
    A0 CAPABILITY
    * CAPABILITY AUTH=PLAIN IMAP4REV1 IMAP4
    A0 OK CAPABILITY Ok
    DEBUG IMAP: AUTH: PLAIN
    DEBUG IMAP: protocolConnect login, host=127.0.0.1, user=admin, password=<non-null>
    DEBUG IMAP: AUTHENTICATE PLAIN command trace suppressed
    AuthenticateRequest{authType=PLAIN}
    DEBUG IMAP: AUTHENTICATE PLAIN command result: A1 OK AUTHENTICATE completed.
    A2 CAPABILITY
    * CAPABILITY AUTH=PLAIN IMAP4REV1 IMAP4
    A2 OK CAPABILITY Ok
    DEBUG IMAP: AUTH: PLAIN
    DEBUG IMAP: connection available -- size: 1
    A3 EXAMINE INBOX

--- src/main/java/de/dhbw/karlsruhe/students/mailflow/core/application/imap/james/JamesImapProcessor.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/core/application/imap/james/JamesImapProcessor.java
@@ -1,30 +1,47 @@
 package de.dhbw.karlsruhe.students.mailflow.core.application.imap.james;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.Random;
 import java.util.Set;
-
+import java.util.stream.Collectors;
+import org.apache.james.core.Username;
 import org.apache.james.imap.api.ImapConfiguration;
 import org.apache.james.imap.api.ImapMessage;
 import org.apache.james.imap.api.display.HumanReadableText;
 import org.apache.james.imap.api.message.Capability;
+import org.apache.james.imap.api.message.request.ImapRequest;
 import org.apache.james.imap.api.message.response.StatusResponseFactory;
 import org.apache.james.imap.api.process.ImapProcessor;
 import org.apache.james.imap.api.process.ImapSession;
 import org.apache.james.imap.message.request.AuthenticateRequest;
 import org.apache.james.imap.message.request.CapabilityRequest;
+import org.apache.james.imap.message.response.AuthenticateResponse;
 import org.apache.james.imap.message.response.CapabilityResponse;
 import org.apache.james.imap.message.response.UnpooledStatusResponseFactory;
+import org.apache.james.mailbox.MailboxManager;
+import org.apache.james.mailbox.MailboxSession;
+import org.apache.james.mailbox.MailboxSession.SessionId;
+import org.apache.james.mailbox.MailboxSession.SessionType;
+import org.apache.james.mailbox.exception.MailboxException;
+import de.dhbw.karlsruhe.students.mailflow.core.util.RandomString;
 
 public class JamesImapProcessor implements ImapProcessor {
 
     private StatusResponseFactory statusResponseFactory;
+    private MailboxManager mailboxManager;
 
     public JamesImapProcessor() {
         this.statusResponseFactory = new UnpooledStatusResponseFactory();
+        this.mailboxManager = new JamesMailboxManager();
     }
 
     @Override
-    public void configure(ImapConfiguration imapConfiguration) {
-    }
+    public void configure(ImapConfiguration imapConfiguration) {}
 
     public void process(ImapMessage message, Responder responder, ImapSession session) {
         if (message instanceof CapabilityRequest) {
@@ -32,20 +49,79 @@ public class JamesImapProcessor implements ImapProcessor {
         } else if (message instanceof AuthenticateRequest) {
             this.processAuthenticateRequest((AuthenticateRequest) message, responder, session);
         } else {
-            this.processReactive(message, responder, session).block();
+            System.out.println(message);
+            // this.processReactive(message, responder, session).block();
         }
     }
 
-    private void processCapabilityRequest(CapabilityRequest message, Responder responder, ImapSession session) {
-        responder.respond(new CapabilityResponse(
-                Set.of(Capability.of("IMAP4"), Capability.of("IMAP4rev1"),
-                        Capability.of("AUTH=PLAIN"))));
-        responder.respond(
-                statusResponseFactory.taggedOk(message.getTag(), message.getCommand(), HumanReadableText.OK));
+    private void processCapabilityRequest(CapabilityRequest message, Responder responder,
+            ImapSession session) {
+        // TODO: Refactor available auth methods away
+        responder.respond(new CapabilityResponse(Set.of(Capability.of("IMAP4"),
+                Capability.of("IMAP4rev1"), Capability.of("AUTH=PLAIN"))));
+        responder.respond(statusResponseFactory.taggedOk(message.getTag(), message.getCommand(),
+                HumanReadableText.OK));
     }
 
-    private void processAuthenticateRequest(AuthenticateRequest message, Responder responder, ImapSession session) {
+    private void processAuthenticateRequest(AuthenticateRequest message, Responder responder,
+            ImapSession session) {
         // TODO: See 6.2.2. AUTHENTICATE Command (RFC 3501)
+
+        // TODO: Refactor to PLAIN-Authentication Strategy pattern
+        System.out.println(message.toString());
+        responder.respond(new AuthenticateResponse());
+        responder.flush();
+        session.pushLineHandler((requestSession, data) -> {
+            try {
+                doPlainAuth(extractInitialClientResponse(data), requestSession, message, responder);
+            } catch (MailboxException e) {
+                e.printStackTrace();
+            }
+            // remove the handler now
+            requestSession.popLineHandler();
+            responder.flush();
+        });
     }
 
+    private static String extractInitialClientResponse(byte[] data) {
+        // cut of the CRLF
+        return new String(data, 0, data.length - 2, StandardCharsets.US_ASCII);
+    }
+
+    /**
+     * Parse the initialClientResponse and do a PLAIN AUTH with it
+     *
+     * @throws MailboxException
+     */
+    protected void doPlainAuth(String initialClientResponse, ImapSession session,
+            ImapRequest request, Responder responder) throws MailboxException {
+
+        String userpass = new String(Base64.getDecoder().decode(initialClientResponse));
+        List<String> tokens = Arrays.stream(userpass.split("\0")).filter(token -> !token.isBlank())
+                .collect(Collectors.toList());
+
+        if (tokens.size() != 2) {
+            throw new MailboxException("invalid login attempt");
+        }
+
+        var username = Username.of(tokens.get(0));
+
+        // TODO: Check if username already exists, then check password or register otherwise
+
+        var sessionId = new Random().nextLong();
+
+        session.setMailboxSession(new MailboxSession(SessionId.of(sessionId), username,
+                Optional.of(username), List.of(Locale.ENGLISH), '/', SessionType.User));
+
+        session.authenticated();
+        session.stopDetectingCommandInjection();
+
+        // send completed
+        responder.respond(statusResponseFactory.taggedOk(request.getTag(), request.getCommand(),
+                HumanReadableText.COMPLETED));
+    }
+
+    public MailboxManager getMailboxManager() {
+        return mailboxManager;
+    }
 }
---
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/core/application/imap/james/JamesMailboxManager.java
@@ -0,0 +1,437 @@
+package de.dhbw.karlsruhe.students.mailflow.core.application.imap.james;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.apache.james.core.Username;
+import org.apache.james.mailbox.MailboxManager;
+import org.apache.james.mailbox.MailboxSession;
+import org.apache.james.mailbox.MessageManager;
+import org.apache.james.mailbox.exception.AnnotationException;
+import org.apache.james.mailbox.exception.MailboxException;
+import org.apache.james.mailbox.model.Mailbox;
+import org.apache.james.mailbox.model.MailboxACL;
+import org.apache.james.mailbox.model.MailboxACL.ACLCommand;
+import org.apache.james.mailbox.model.MailboxACL.EntryKey;
+import org.apache.james.mailbox.model.MailboxACL.Rfc4314Rights;
+import org.apache.james.mailbox.model.MailboxACL.Right;
+import org.apache.james.mailbox.model.MailboxAnnotation;
+import org.apache.james.mailbox.model.MailboxAnnotationKey;
+import org.apache.james.mailbox.model.MailboxId;
+import org.apache.james.mailbox.model.MailboxMetaData;
+import org.apache.james.mailbox.model.MailboxPath;
+import org.apache.james.mailbox.model.MessageId;
+import org.apache.james.mailbox.model.MessageId.Factory;
+import org.apache.james.mailbox.model.MessageRange;
+import org.apache.james.mailbox.model.MultimailboxesSearchQuery;
+import org.apache.james.mailbox.model.ThreadId;
+import org.apache.james.mailbox.model.search.MailboxQuery;
+import org.reactivestreams.Publisher;
+
+import reactor.core.publisher.Flux;
+
+public class JamesMailboxManager implements MailboxManager {
+
+    @Override
+    public void startProcessingRequest(MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startProcessingRequest'");
+    }
+
+    @Override
+    public void endProcessingRequest(MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'endProcessingRequest'");
+    }
+
+    @Override
+    public boolean hasRight(MailboxPath mailboxPath, Right right, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasRight'");
+    }
+
+    @Override
+    public Publisher<Boolean> hasRightReactive(MailboxPath mailboxPath, Right right,
+            MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasRightReactive'");
+    }
+
+    @Override
+    public boolean hasRight(Mailbox mailbox, Right right, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasRight'");
+    }
+
+    @Override
+    public boolean hasRight(MailboxId mailboxId, Right right, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasRight'");
+    }
+
+    @Override
+    public List<Rfc4314Rights> listRights(MailboxPath mailboxPath, EntryKey identifier,
+            MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listRights'");
+    }
+
+    @Override
+    public List<Rfc4314Rights> listRights(Mailbox mailbox, EntryKey identifier,
+            MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listRights'");
+    }
+
+    @Override
+    public MailboxACL listRights(MailboxPath mailboxPath, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listRights'");
+    }
+
+    @Override
+    public MailboxACL listRights(MailboxId mailboxId, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listRights'");
+    }
+
+    @Override
+    public Rfc4314Rights myRights(MailboxPath mailboxPath, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'myRights'");
+    }
+
+    @Override
+    public Publisher<Rfc4314Rights> myRightsReactive(MailboxPath mailboxPath,
+            MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'myRightsReactive'");
+    }
+
+    @Override
+    public Publisher<Rfc4314Rights> myRights(MailboxId mailboxId, MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'myRights'");
+    }
+
+    @Override
+    public Rfc4314Rights myRights(Mailbox mailbox, MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'myRights'");
+    }
+
+    @Override
+    public void applyRightsCommand(MailboxPath mailboxPath, ACLCommand mailboxACLCommand,
+            MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'applyRightsCommand'");
+    }
+
+    @Override
+    public Publisher<Void> applyRightsCommandReactive(MailboxPath mailboxPath,
+            ACLCommand mailboxACLCommand, MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+                "Unimplemented method 'applyRightsCommandReactive'");
+    }
+
+    @Override
+    public void applyRightsCommand(MailboxId mailboxId, ACLCommand mailboxACLCommand,
+            MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'applyRightsCommand'");
+    }
+
+    @Override
+    public void setRights(MailboxPath mailboxPath, MailboxACL mailboxACL, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setRights'");
+    }
+
+    @Override
+    public void setRights(MailboxId mailboxId, MailboxACL mailboxACL, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setRights'");
+    }
+
+    @Override
+    public List<MailboxAnnotation> getAllAnnotations(MailboxPath mailboxPath,
+            MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllAnnotations'");
+    }
+
+    @Override
+    public Publisher<MailboxAnnotation> getAllAnnotationsReactive(MailboxPath mailboxPath,
+            MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllAnnotationsReactive'");
+    }
+
+    @Override
+    public List<MailboxAnnotation> getAnnotationsByKeys(MailboxPath mailboxPath,
+            MailboxSession session, Set<MailboxAnnotationKey> keys) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAnnotationsByKeys'");
+    }
+
+    @Override
+    public Publisher<MailboxAnnotation> getAnnotationsByKeysReactive(MailboxPath mailboxPath,
+            MailboxSession session, Set<MailboxAnnotationKey> keys) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+                "Unimplemented method 'getAnnotationsByKeysReactive'");
+    }
+
+    @Override
+    public List<MailboxAnnotation> getAnnotationsByKeysWithOneDepth(MailboxPath mailboxPath,
+            MailboxSession session, Set<MailboxAnnotationKey> keys) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+                "Unimplemented method 'getAnnotationsByKeysWithOneDepth'");
+    }
+
+    @Override
+    public Publisher<MailboxAnnotation> getAnnotationsByKeysWithOneDepthReactive(
+            MailboxPath mailboxPath, MailboxSession session, Set<MailboxAnnotationKey> keys) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+                "Unimplemented method 'getAnnotationsByKeysWithOneDepthReactive'");
+    }
+
+    @Override
+    public List<MailboxAnnotation> getAnnotationsByKeysWithAllDepth(MailboxPath mailboxPath,
+            MailboxSession session, Set<MailboxAnnotationKey> keys) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+                "Unimplemented method 'getAnnotationsByKeysWithAllDepth'");
+    }
+
+    @Override
+    public Publisher<MailboxAnnotation> getAnnotationsByKeysWithAllDepthReactive(
+            MailboxPath mailboxPath, MailboxSession session, Set<MailboxAnnotationKey> keys) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+                "Unimplemented method 'getAnnotationsByKeysWithAllDepthReactive'");
+    }
+
+    @Override
+    public void updateAnnotations(MailboxPath mailboxPath, MailboxSession session,
+            List<MailboxAnnotation> mailboxAnnotations)
+            throws MailboxException, AnnotationException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateAnnotations'");
+    }
+
+    @Override
+    public Publisher<Void> updateAnnotationsReactive(MailboxPath mailboxPath,
+            MailboxSession session, List<MailboxAnnotation> mailboxAnnotations) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateAnnotationsReactive'");
+    }
+
+    @Override
+    public MailboxSession createSystemSession(Username userName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createSystemSession'");
+    }
+
+    @Override
+    public AuthorizationStep authenticate(Username givenUserid, String passwd) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'authenticate'");
+    }
+
+    @Override
+    public AuthorizationStep authenticate(Username givenUserid) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'authenticate'");
+    }
+
+    @Override
+    public EnumSet<MailboxCapabilities> getSupportedMailboxCapabilities() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+                "Unimplemented method 'getSupportedMailboxCapabilities'");
+    }
+
+    @Override
+    public boolean hasCapability(MailboxCapabilities capability) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasCapability'");
+    }
+
+    @Override
+    public EnumSet<MessageCapabilities> getSupportedMessageCapabilities() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+                "Unimplemented method 'getSupportedMessageCapabilities'");
+    }
+
+    @Override
+    public EnumSet<SearchCapabilities> getSupportedSearchCapabilities() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException(
+                "Unimplemented method 'getSupportedSearchCapabilities'");
+    }
+
+    @Override
+    public MessageManager getMailbox(MailboxPath mailboxPath, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMailbox'");
+    }
+
+    @Override
+    public MessageManager getMailbox(MailboxId mailboxId, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMailbox'");
+    }
+
+    @Override
+    public Publisher<MessageManager> getMailboxReactive(MailboxId mailboxId,
+            MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMailboxReactive'");
+    }
+
+    @Override
+    public Publisher<MessageManager> getMailboxReactive(MailboxPath mailboxPath,
+            MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMailboxReactive'");
+    }
+
+    @Override
+    public MessageManager getMailbox(Mailbox mailbox, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMailbox'");
+    }
+
+    @Override
+    public Optional<MailboxId> createMailbox(MailboxPath mailboxPath, CreateOption createOption,
+            MailboxSession mailboxSession) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createMailbox'");
+    }
+
+    @Override
+    public void deleteMailbox(MailboxPath mailboxPath, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteMailbox'");
+    }
+
+    @Override
+    public Mailbox deleteMailbox(MailboxId mailboxId, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteMailbox'");
+    }
+
+    @Override
+    public List<MailboxRenamedResult> renameMailbox(MailboxPath from, MailboxPath to,
+            RenameOption option, MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'renameMailbox'");
+    }
+
+    @Override
+    public List<MailboxRenamedResult> renameMailbox(MailboxId mailboxId, MailboxPath newMailboxPath,
+            RenameOption option, MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'renameMailbox'");
+    }
+
+    @Override
+    public List<MessageRange> copyMessages(MessageRange set, MailboxPath from, MailboxPath to,
+            MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'copyMessages'");
+    }
+
+    @Override
+    public List<MessageRange> copyMessages(MessageRange set, MailboxId from, MailboxId to,
+            MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'copyMessages'");
+    }
+
+    @Override
+    public List<MessageRange> moveMessages(MessageRange set, MailboxPath from, MailboxPath to,
+            MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveMessages'");
+    }
+
+    @Override
+    public List<MessageRange> moveMessages(MessageRange set, MailboxId from, MailboxId to,
+            MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveMessages'");
+    }
+
+    @Override
+    public Flux<MailboxMetaData> search(MailboxQuery expression, MailboxSearchFetchType fetchType,
+            MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
+
+    @Override
+    public Publisher<MessageId> search(MultimailboxesSearchQuery expression, MailboxSession session,
+            long limit) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
+
+    @Override
+    public Publisher<MessageId> getThread(ThreadId threadId, MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getThread'");
+    }
+
+    @Override
+    public Publisher<Boolean> mailboxExists(MailboxPath mailboxPath, MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mailboxExists'");
+    }
+
+    @Override
+    public List<MailboxPath> list(MailboxSession session) throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'list'");
+    }
+
+    @Override
+    public boolean hasChildren(MailboxPath mailboxPath, MailboxSession session)
+            throws MailboxException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasChildren'");
+    }
+
+    @Override
+    public Publisher<Boolean> hasChildrenReactive(MailboxPath mailboxPath, MailboxSession session) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasChildrenReactive'");
+    }
+
+    @Override
+    public Factory getMessageIdFactory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMessageIdFactory'");
+    }
+
+}
