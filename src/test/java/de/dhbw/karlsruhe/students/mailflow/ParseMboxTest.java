d4a89bda14bc4364b7b74f7f94b475f503581d57
Cebox82 <seifert_lars@gmx.net>
2024-03-12 15:12:26 +0100

    add test for parser

--- src/test/java/de/dhbw/karlsruhe/students/mailflow/ParseMboxTest.java
+++ src/test/java/de/dhbw/karlsruhe/students/mailflow/ParseMboxTest.java
@@ -1,25 +1,74 @@
 package de.dhbw.karlsruhe.students.mailflow;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import de.dhbw.karlsruhe.students.mailflow.core.application.email.MailboxRepository;
+import de.dhbw.karlsruhe.students.mailflow.core.application.email.parsing.MailboxParser;
 import de.dhbw.karlsruhe.students.mailflow.core.application.email.parsing.MailboxParsingService;
 import de.dhbw.karlsruhe.students.mailflow.core.application.email.parsing.MailboxParsingServiceException;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.Email;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.email.Mailbox;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects.Address;
+import de.dhbw.karlsruhe.students.mailflow.external.infrastructure.email.CreateEmailHelper;
 import de.dhbw.karlsruhe.students.mailflow.external.infrastructure.email.LocalMailboxRepository;
 import de.dhbw.karlsruhe.students.mailflow.external.infrastructure.email.MboxParser;
+import jakarta.mail.Folder;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class ParseMboxTest {
 
+  @Mock
+  private Session sessionMock;
+
+  @Mock
+  private Folder folderMock;
+
+  @Mock
+  private Message messageMock;
+
+  @Mock
+  private Email emailMock;
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void testParsedMailboxSuccess() throws MessagingException {
+    // Arrange
+    List<Message> messageList = new ArrayList<>();
+    messageList.add(messageMock);
+    when(folderMock.getMessages()).thenReturn(messageList.toArray(new Message[0]));
+    when(CreateEmailHelper.createEmailWithMessage(messageMock)).thenReturn(emailMock);
+
+    MailboxParser parser = new MboxParser();
+
+    File mailboxFile = new File("Daily_Problem.mbox");
+    Address address = new Address("test", "gmail.com");
+
+    // Act
+    Mailbox mailbox = parser.parseMailbox(mailboxFile, address);
+
+    // Assert (Verify expected behavior)
+    assertEquals(1, mailbox.getEmails().size());
+    verify(sessionMock, times(1)).getInstance(System.getProperties(), null);
+    verify(folderMock, times(1)).open(Folder.READ_ONLY);
+  }
   @Test
   public void parseMboxFileToMailbox() throws MailboxParsingServiceException {
 
@@ -43,9 +92,6 @@ public class ParseMboxTest {
 
     // Service (use case)
     Mailbox mailbox = mailboxParsingService.getMailboxOfAddress(address);
-
-
-    // Parser
 
 
     // Assert
