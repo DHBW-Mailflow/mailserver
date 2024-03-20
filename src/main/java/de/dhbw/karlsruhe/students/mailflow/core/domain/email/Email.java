1f137c86eb1556f3e2423eac0b8fdf1ee935365c
Jonas-Karl <jonas.karl.rade@gmail.com>
2024-03-20 23:40:34 +0100

    fix(json parsing): Date issues in tests

--- src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/email/Email.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/email/Email.java
@@ -12,10 +12,10 @@ import de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects.Email
  * Representation of an e-mail as AggregateRoot
  */
 public final class Email extends AggregateRoot<EmailId> {
-    private EmailMetadata emailMetadata;
+    private final EmailMetadata emailMetadata;
     private boolean isRead;
-    private String content;
-    private Set<Attachment> attachments;
+    private final String content;
+    private final Set<Attachment> attachments;
 
     private Email(EmailId id, String content, EmailMetadata emailMetadata,
             Set<Attachment> attachments) {
