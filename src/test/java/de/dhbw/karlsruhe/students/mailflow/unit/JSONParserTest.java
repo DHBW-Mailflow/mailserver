83a062036b87a4ba15ac7cc5ad759bb5ff6fc6db
Jonas-Karl <jonas.karl.rade@gmail.com>
2024-03-20 22:01:26 +0100

    cleanup(json parsing): rename files

--- src/test/java/de/dhbw/karlsruhe/students/mailflow/unit/JSONParserTest.java
+++ src/test/java/de/dhbw/karlsruhe/students/mailflow/unit/JSONParserTest.java
@@ -19,7 +19,7 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ParseLocalJSONMailboxTest {
+public class JSONParserTest {
   @Test
   public void successfullyParseJsonToMailbox() {
     Address owner = new Address("anotherUser", "anotherDomain.de");
