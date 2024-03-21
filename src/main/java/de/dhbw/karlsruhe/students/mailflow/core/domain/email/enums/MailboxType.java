3cbe59fcc35ebdf6f0c5326e0d17a370a7d37ea2
Jonas-Karl <jonas.karl.rade@gmail.com>
2024-03-21 15:53:04 +0100

    fix(json parsing): enhance docs of MailboxType

--- src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/email/enums/MailboxType.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/email/enums/MailboxType.java
@@ -1,29 +1,40 @@
 package de.dhbw.karlsruhe.students.mailflow.core.domain.email.enums;
 
-/**
- * The type a mailbox can have.
- * The SENT mailbox type for emails which the user has already sent.
- * The READ mailbox type for emails which the user received from others and has already read.
- * The UNREAD mailbox type for emails which the user received from others and are not already read.
- * The SPAM_READ mailbox type for emails which where categorized as SPAM and are read.
- * The SPAM_UNREAD mailbox type for emails which where categorized as SPAM and are not already read.
- * The DELETED mailbox type for emails which where deleted by the user. They will be removed in the future.
- * The DRAFT mailbox type for emails which the user created but did not yet send.
- */
+/** The type a mailbox can have. This is used to categorize emails. */
 public enum MailboxType {
 
+  /** The SENT mailbox type for emails which the user has already sent. */
   SENT("sent"),
+
+  /** The READ mailbox type for emails which the user received from others and has already read. */
   READ("read"),
+
+  /**
+   * The UNREAD mailbox type for emails which the user received from others and are not already read
+   */
   UNREAD("unread"),
+
+  /** The SPAM_READ mailbox type for emails which where categorized as SPAM and are read. */
   SPAM_READ("spam-read"),
+
+  /**
+   * The SPAM_UNREAD mailbox type for emails which where categorized as SPAM and are not already
+   * read.
+   */
   SPAM_UNREAD("spam-unread"),
+
+  /**
+   * The DELETED mailbox type for emails which where deleted by the user. They will be removed in
+   * the future.
+   */
   DELETED("deleted"),
+
+  /** The DRAFT mailbox type for emails which the user created but did not yet send. */
   DRAFT("draft");
 
-/**
-*
- * @param fileSuffix fitting to an enum value.
-*/
+  /**
+   * @param fileSuffix defined by an enum value.
+   */
   MailboxType(String fileSuffix) {
     this.fileSuffix = fileSuffix.toLowerCase();
   }
