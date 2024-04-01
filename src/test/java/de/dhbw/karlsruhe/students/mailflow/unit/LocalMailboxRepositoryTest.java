0b58e8620ad2f5e48f4a80de86a18a183f3b4ff1
seiferla <seifert_lars@gmx.net>
2024-04-01 16:46:37 +0200

    fix localmailboxtests

--- src/test/java/de/dhbw/karlsruhe/students/mailflow/unit/LocalMailboxRepositoryTest.java
+++ src/test/java/de/dhbw/karlsruhe/students/mailflow/unit/LocalMailboxRepositoryTest.java
@@ -3,6 +3,7 @@ package de.dhbw.karlsruhe.students.mailflow.unit;
 import de.dhbw.karlsruhe.students.mailflow.core.application.email.MailboxRepository;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.email.enums.MailboxType;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects.Address;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.user.User;
 import de.dhbw.karlsruhe.students.mailflow.external.infrastructure.email.LocalMailboxRepository;
 import java.io.File;
 import java.util.Optional;
@@ -17,6 +18,7 @@ class LocalMailboxRepositoryTest {
   void retrieveFromCorrectPath(MailboxType mailboxType) {
     // Arrange
     Address mailboxOwner = new Address("someOwner", "someDomain.de");
+    User user = new User(mailboxOwner, "someDomain.de", "somePassword");
 
     // Needed to test the path of a non-existing file in test environment
     MailboxRepository ownMailboxRepository =
@@ -24,7 +26,7 @@ class LocalMailboxRepositoryTest {
             Optional.of(LocalMailboxRepository.getFile(userAddress, type));
 
     Optional<File> mailboxFile =
-        ownMailboxRepository.provideStoredMailboxFileFor(mailboxOwner, mailboxType);
+        ownMailboxRepository.provideStoredMailboxFileFor(user, mailboxType);
 
     // Act
     String path = mailboxFile.get().getPath();
@@ -41,12 +43,13 @@ class LocalMailboxRepositoryTest {
   void returnEmptyOptionalWithNonExistingFile(MailboxType mailboxType) {
     // Arrange
     Address mailboxOwner = new Address("someOwner", "someDomain.de");
+    User user = new User(mailboxOwner, "someDomain.de", "somePassword");
 
     // Needed to test the path of a non-existing file
     MailboxRepository repository = new LocalMailboxRepository();
 
     // Act
-    Optional<File> mailboxFile = repository.provideStoredMailboxFileFor(mailboxOwner, mailboxType);
+    Optional<File> mailboxFile = repository.provideStoredMailboxFileFor(user, mailboxType);
 
     // Assert
     Assertions.assertThat(mailboxFile.isEmpty()).isTrue();
