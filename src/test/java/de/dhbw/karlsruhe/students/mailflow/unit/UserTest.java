e99931fce4ca24b7fc9f93f400fca54f93e1bbf0
seiferla <seifert_lars@gmx.net>
2024-04-01 17:19:00 +0200

    add user test class

--- src/test/java/de/dhbw/karlsruhe/students/mailflow/unit/UserTest.java
+++ src/test/java/de/dhbw/karlsruhe/students/mailflow/unit/UserTest.java
@@ -1,12 +1,16 @@
 package de.dhbw.karlsruhe.students.mailflow.unit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects.Address;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.user.InvalidEmailException;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.user.InvalidPasswordException;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.user.InvalidSaltException;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.user.User;
 import org.junit.jupiter.api.Test;
 
-public class UserTest {
+class UserTest {
 
   @Test
   void testUserCreation() {
@@ -22,6 +26,45 @@ public class UserTest {
     assertEquals(email, user.email());
     assertEquals(password, user.password());
     assertEquals(salt, user.salt());
+  }
+
+  @Test
+  void testUserCreationWithInvalidEmail() {
+    // Arrange
+    Address email = null;
+    String password = "password";
+    String salt = "salt";
+
+    // Act
+
+    // Assert
+    assertThrows(InvalidEmailException.class, () -> new User(email, password, salt));
+  }
+
+  @Test
+  void testUserCreationWithInvalidPassword() {
+    // Arrange
+    Address email = new Address("test", "example.com");
+    String password = null;
+    String salt = "salt";
+
+    // Act
+
+    // Assert
+    assertThrows(InvalidPasswordException.class, () -> new User(email, password, salt));
+  }
+
+  @Test
+  void testUserCreationWithInvalidSalt() {
+    // Arrange
+    Address email = new Address("test", "example.com");
+    String password = "password";
+    String salt = null;
+
+    // Act
+
+    // Assert
+    assertThrows(InvalidSaltException.class, () -> new User(email, password, salt));
   }
 
 }
