ab03591edae4e0a01925e9a2a4b7be061fe1da80
Jonas-Karl <jonas.karl.rade@gmail.com>
2024-04-10 01:38:29 +0200

    feature(smtp-listener): at least start smtp server. Throws unknown command

--- src/main/java/de/dhbw/karlsruhe/students/mailflow/App.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/App.java
@@ -3,14 +3,36 @@ package de.dhbw.karlsruhe.students.mailflow;
 import de.dhbw.karlsruhe.students.mailflow.core.application.imap.ListenerService;
 import de.dhbw.karlsruhe.students.mailflow.core.application.imap.LocalListenerService;
 import de.dhbw.karlsruhe.students.mailflow.external.infrastructure.james.JamesImapListener;
+import java.net.InetSocketAddress;
+import org.apache.james.metrics.api.MetricFactory;
+import org.apache.james.metrics.logger.DefaultMetricFactory;
+import org.apache.james.protocols.api.handler.ProtocolHandlerChain;
+import org.apache.james.protocols.netty.NettyServer;
+import org.apache.james.protocols.smtp.SMTPConfiguration;
+import org.apache.james.protocols.smtp.SMTPConfigurationImpl;
+import org.apache.james.protocols.smtp.SMTPProtocol;
+import org.apache.james.protocols.smtp.SMTPProtocolHandlerChain;
 
 /**
  * Hello world!
- * 
+ *
  * @author jens1o, Jonas-Karl, seiferla
  */
 public class App {
   public static void main(String[] args) throws Exception {
+    int smtpPort = 50200;
+    String smtpHost = "localhost";
+
+    SMTPConfiguration smtpConfiguration = new SMTPConfigurationImpl();
+    MetricFactory metricFactory = new DefaultMetricFactory();
+    ProtocolHandlerChain chain = new SMTPProtocolHandlerChain(metricFactory);
+
+    NettyServer server =
+        new NettyServer.Factory().protocol(new SMTPProtocol(chain, smtpConfiguration)).build();
+    server.setListenAddresses(new InetSocketAddress(smtpHost, smtpPort));
+    server.bind();
+    System.out.printf("Server started with addresses: %s", server.getListenAddresses().toString());
+
     ListenerService listenerService = new LocalListenerService(new JamesImapListener());
     listenerService.listen();
   }
--- src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/imap/ImapListenerConfig.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/imap/ImapListenerConfig.java
@@ -16,7 +16,7 @@ public record ImapListenerConfig(String host, int port) {
    * @throws IOException if there occurred an issue during the port allocation
    */
   private static int getFreePort() throws IOException {
-    try (ServerSocket socket = new ServerSocket(0)) {
+    try (ServerSocket socket = new ServerSocket(50300)) {
       var port = socket.getLocalPort();
 
       if (port != -1) {
