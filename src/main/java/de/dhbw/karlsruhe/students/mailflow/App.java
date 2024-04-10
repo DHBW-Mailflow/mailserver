7ea57af57b5b89bb995bdfbdf466ce732d1cb847
Jonas-Karl <jonas.karl.rade@gmail.com>
2024-04-10 10:50:00 +0200

    feature(smtp-listener): enable logging with debug in james

--- src/main/java/de/dhbw/karlsruhe/students/mailflow/App.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/App.java
@@ -12,6 +12,8 @@ import org.apache.james.protocols.smtp.SMTPConfiguration;
 import org.apache.james.protocols.smtp.SMTPConfigurationImpl;
 import org.apache.james.protocols.smtp.SMTPProtocol;
 import org.apache.james.protocols.smtp.SMTPProtocolHandlerChain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Hello world!
@@ -19,7 +21,13 @@ import org.apache.james.protocols.smtp.SMTPProtocolHandlerChain;
  * @author jens1o, Jonas-Karl, seiferla
  */
 public class App {
+  private static final Logger logger = LoggerFactory.getLogger(App.class);
+
   public static void main(String[] args) throws Exception {
+
+    logger.info("Starting SMTP...");
+
+
     int smtpPort = 50200;
     String smtpHost = "localhost";
     SMTPConfiguration smtpConfiguration = new SMTPConfigurationImpl();
@@ -30,7 +38,7 @@ public class App {
         new NettyServer.Factory().protocol(new SMTPProtocol(chain, smtpConfiguration)).build();
     server.setListenAddresses(new InetSocketAddress(smtpHost, smtpPort));
     server.bind();
-    System.out.printf("Server started with addresses: %s", server.getListenAddresses().toString());
+    logger.info("SMTP Server started with addresses: %s".formatted(server.getListenAddresses().toString()));
 
     ListenerService listenerService = new LocalListenerService(new JamesImapListener());
     listenerService.listen();
