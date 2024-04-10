8e24a3426b038e61991bc3f66355c234c6f24265
Jonas-Karl <jonas.karl.rade@gmail.com>
2024-04-10 02:06:30 +0200

    feature(smtp-listener): enable logging in james server

--- src/main/java/de/dhbw/karlsruhe/students/mailflow/App.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/App.java
@@ -22,7 +22,6 @@ public class App {
   public static void main(String[] args) throws Exception {
     int smtpPort = 50200;
     String smtpHost = "localhost";
-
     SMTPConfiguration smtpConfiguration = new SMTPConfigurationImpl();
     MetricFactory metricFactory = new DefaultMetricFactory();
     ProtocolHandlerChain chain = new SMTPProtocolHandlerChain(metricFactory);
