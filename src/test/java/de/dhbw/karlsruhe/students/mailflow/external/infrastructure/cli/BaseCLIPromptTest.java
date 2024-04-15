7c4181f47c7c8add0792a8b4dd038e6c2ebd1d1e
jens1o <mail@jens-hausdorf.de>
2024-04-15 10:38:29 +0200

    tests: Fix test to work

--- src/test/java/de/dhbw/karlsruhe/students/mailflow/external/infrastructure/cli/BaseCLIPromptTest.java
+++ src/test/java/de/dhbw/karlsruhe/students/mailflow/external/infrastructure/cli/BaseCLIPromptTest.java
@@ -1,33 +1,37 @@
 package de.dhbw.karlsruhe.students.mailflow.external.infrastructure.cli;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BaseCLIPromptTest {
   private BaseCLIPrompt baseCLIPrompt;
-
-  @BeforeEach
-  void setUp() {
-    baseCLIPrompt = new BaseCLIPrompt();
-    baseCLIPrompt.start();
-  }
+  private static final InputStream originalStandardIn = System.in;
 
   @AfterEach
   void tearDown() {
-    baseCLIPrompt.stop();
-    System.setIn(System.in);
+    System.setIn(originalStandardIn);
   }
 
   @Test
   void readUserInputWithOptions() {
-    System.setIn(new ByteArrayInputStream("1".getBytes()));
+    // Arrange
+    System.setIn(new ByteArrayInputStream("1\n".getBytes()));
+
+    baseCLIPrompt = new BaseCLIPrompt();
+    baseCLIPrompt.start();
+
+    // Act
     Map<String, BaseCLIPrompt> options = new HashMap<>();
     options.put("First option", new BaseCLIPrompt());
     options.put("Second option", new BaseCLIPrompt());
     baseCLIPrompt.readUserInputWithOptions(options);
+
+    // Assert
+    assertTrue(true);
   }
 }
