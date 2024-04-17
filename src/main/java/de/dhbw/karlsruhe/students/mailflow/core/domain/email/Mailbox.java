de4152cc994f57280c66565e182ff8ee3b9a5f59
jens1o <mail@jens-hausdorf.de>
2024-04-17 21:17:44 +0200

    fix: Minor clean-up
    It is redundant to remove an entry when putting it into a map again, see https://docs.oracle.com/javase/8/docs/api/java/util/Map.html#put-K-V-

--- src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/email/Mailbox.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/core/domain/email/Mailbox.java
@@ -45,11 +45,9 @@ public final class Mailbox extends AggregateRoot<MailboxId> {
     this.emails.put(email, Set.of(isUnread ? Label.UNREAD : Label.READ));
   }
 
-  public List<Email> getEmailsWithLabel(Label... labels){
-    return emails.entrySet().stream()
-        .filter(entry -> entry.getValue().containsAll(Set.of(labels)))
-        .map(Map.Entry::getKey)
-        .toList();
+  public List<Email> getEmailsWithLabel(Label... labels) {
+    return emails.entrySet().stream().filter(entry -> entry.getValue().containsAll(Set.of(labels)))
+        .map(Map.Entry::getKey).toList();
   }
 
   @Override
@@ -77,7 +75,6 @@ public final class Mailbox extends AggregateRoot<MailboxId> {
   }
 
   public void markWithLabel(Email email, Label... label) {
-    emails.remove(email);
     emails.put(email, Set.of(label));
   }
 }
