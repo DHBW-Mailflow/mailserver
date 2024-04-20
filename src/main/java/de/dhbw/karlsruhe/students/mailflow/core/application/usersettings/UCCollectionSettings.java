34bdf74569c3ec340ee72395f4cca395bf73e1e6
seiferla <seifert_lars@gmx.net>
2024-04-20 14:46:18 +0200

    create UCCollectionSettings

--- src/main/java/de/dhbw/karlsruhe/students/mailflow/core/application/usersettings/UCCollectionSettings.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/core/application/usersettings/UCCollectionSettings.java
@@ -1,3 +1,15 @@
 package de.dhbw.karlsruhe.students.mailflow.core.application.usersettings;
 
-public class UCCollectionSettings {}
+import de.dhbw.karlsruhe.students.mailflow.core.application.auth.AuthSessionUseCase;
+import de.dhbw.karlsruhe.students.mailflow.core.application.usersettings.changesignature.ChangeSignatureService;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.MailboxRepository;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.user.UserSettingsRepository;
+
+public record UCCollectionSettings(ChangeSignatureService changeSignatureService) {
+
+  public static UCCollectionSettings init(
+      AuthSessionUseCase authSession, UserSettingsRepository userSettingsRepository) {
+    return new UCCollectionSettings(
+        new ChangeSignatureService(userSettingsRepository, authSession));
+  }
+}
--- src/main/java/de/dhbw/karlsruhe/students/mailflow/core/application/usersettings/changesignature/ChangeSignatureService.java
+++ src/main/java/de/dhbw/karlsruhe/students/mailflow/core/application/usersettings/changesignature/ChangeSignatureService.java
@@ -1,13 +1,17 @@
 package de.dhbw.karlsruhe.students.mailflow.core.application.usersettings.changesignature;
 
+import de.dhbw.karlsruhe.students.mailflow.core.application.auth.AuthSessionUseCase;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.user.UserSettingsRepository;
 
 public class ChangeSignatureService implements ChangeSignatureUseCase {
 
   private final UserSettingsRepository userSettingsRepository;
+  private final AuthSessionUseCase authSession;
 
-  public ChangeSignatureService(UserSettingsRepository userSettingsRepository) {
+  public ChangeSignatureService(UserSettingsRepository userSettingsRepository,
+      AuthSessionUseCase authSession) {
     this.userSettingsRepository = userSettingsRepository;
+    this.authSession = authSession;
   }
 
   @Override
