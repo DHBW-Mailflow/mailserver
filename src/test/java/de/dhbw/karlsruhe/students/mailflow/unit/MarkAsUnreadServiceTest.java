58a53cb974c0e613c5ecd1822d93ae4aa6d95655
Jonas-Karl <jonas.karl.rade@gmail.com>
2024-04-24 11:54:40 +0200

    - introduce authSession mock class - MarkAsReadServiceTest - MarkAsUnreadServiceTest

---
+++ src/test/java/de/dhbw/karlsruhe/students/mailflow/unit/MarkAsUnreadServiceTest.java
@@ -0,0 +1,68 @@
+package de.dhbw.karlsruhe.students.mailflow.unit;
+
+import de.dhbw.karlsruhe.students.mailflow.core.application.auth.AuthSessionUseCase;
+import de.dhbw.karlsruhe.students.mailflow.core.application.email.organize.MarkAsUnreadService;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.Email;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.Mailbox;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.MailboxRepository;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.enums.Label;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.enums.MailboxType;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.exceptions.MailboxLoadingException;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.exceptions.MailboxSavingException;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects.Address;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects.EmailMetadata;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects.SentDate;
+import de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects.Subject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.fest.assertions.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MarkAsUnreadServiceTest {
+  @Test
+  public void testMarkAsUnread() throws MailboxSavingException, MailboxLoadingException {
+    // Arrange
+    Address loggedInUser = new Address("some", "domain.de");
+    EmailMetadata emailMetadata =
+        new EmailMetadata(
+            new Subject("someSubject"), loggedInUser, List.of(), null, SentDate.ofNow());
+    Email emailToMark = Email.create("content", emailMetadata);
+    Map<Email, Set<Label>> mailboxEmails = new HashMap<>();
+    mailboxEmails.put(emailToMark, Set.of(Label.READ));
+    Mailbox mailboxOfUser = Mailbox.create(loggedInUser, mailboxEmails, MailboxType.INBOX);
+
+    MarkAsUnreadService markAsReadService = getMarkAsUnreadService(loggedInUser, mailboxOfUser);
+
+    // Act
+    markAsReadService.mark(emailToMark);
+    List<Email> readEmails = mailboxOfUser.getEmailsWithLabel(Label.UNREAD);
+
+    // Assert
+    Assertions.assertThat(readEmails).contains(emailToMark);
+  }
+
+  private static MarkAsUnreadService getMarkAsUnreadService(
+      Address loggedInUser, Mailbox mailboxOfUser) {
+    AuthSessionUseCase mockedAuthSession = new MockedAuthorizedSession(loggedInUser);
+
+    MailboxRepository mockedMailboxRepository =
+        new MailboxRepository() {
+          @Override
+          public Mailbox findByAddressAndType(Address address, MailboxType type) {
+            return mailboxOfUser;
+          }
+
+          @Override
+          public void save(Mailbox mailbox) {}
+
+          @Override
+          public List<Mailbox> findAllOtherInboxes(Address sender) {
+            return List.of();
+          }
+        };
+
+    return new MarkAsUnreadService(mockedAuthSession, mockedMailboxRepository);
+  }
+}
