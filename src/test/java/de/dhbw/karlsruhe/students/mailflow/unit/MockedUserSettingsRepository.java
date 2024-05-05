99c8a8ced148ea59dbb03071c62dbd19a9db26a8
seiferla <seifert_lars@gmx.net>
2024-05-06 00:17:54 +0200

    sonarlint unused imports

--- src/test/java/de/dhbw/karlsruhe/students/mailflow/unit/MockedUserSettingsRepository.java
+++ src/test/java/de/dhbw/karlsruhe/students/mailflow/unit/MockedUserSettingsRepository.java
@@ -3,8 +3,7 @@ package de.dhbw.karlsruhe.students.mailflow.unit;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.email.value_objects.Address;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.user.UserSettings;
 import de.dhbw.karlsruhe.students.mailflow.core.domain.user.UserSettingsRepository;
-import de.dhbw.karlsruhe.students.mailflow.core.domain.user.exceptions.LoadSettingsException;
-import de.dhbw.karlsruhe.students.mailflow.core.domain.user.exceptions.SaveSettingsException;
+
 
 public class MockedUserSettingsRepository implements UserSettingsRepository {
 
